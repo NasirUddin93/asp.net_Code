c766f86ad0c9af489c404a46e84060b98b2599d8
Nasir Uddin <nasir93cse@gmail.com>
2020-03-24 00:04:24 +0600

    step - 02

--- Important_Code.cs
+++ Important_Code.cs
@@ -20,3 +20,49 @@
         {
             return Content(year+"/"+month);
         }
+////////////////////////////////////////////////////////////
+
+        public ActionResult index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (string.IsNullOrWhiteSpace(sortBy))
+
+            {
+                sortBy = "Name";
+            }
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+///////////////////////////////////////Checking return type //////////////////////////////
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!", Id = 101, Description = "This is good movie" };
+            //return View(movie);
+            //return Content("Hello world!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page = 1, sortByName = "Ahsan" });
+        }
+///////////////////////////////View file////////////////////////////////////////////////
+@model Vidly03.Models.Movie
+@{
+    ViewBag.Title = "Random";
+}
+
+<h2>@Model.Name</h2>
+///////////////////////////////////////////////////////////////////////////////////////
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "ASPNETMVC5ATM1";
+            if (letterCase == "lower")
+            {
+                return Content(serial.ToLower());
+            }
+            return Content(serial);
+        }
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
